e7061ddf42d078ddf51738e5f37b6f919c481630
CCastro <chris502castro@gmail.com>
2018-11-17 14:24:52 -0700

    Answer values are pushed into an array which later is checked to see if it has the same values as the correct answers array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
+function assignValues() {
+    $("#1").attr("value", 1);
+    $("#2").attr("value", 2);
+    $("#3").attr("value", 3);
+    $("#4").attr("value", 4);
+}
+
+assignValues();
+
 // Array of arrays; each sub-array holds the question first, and the 4 multiple choice answers
 var qAndA = [["question1", "answer1", "answer2", "answer3", "answer4"],
 ["question2", "answer1", "answer2", "answer3", "answer4"],
@@ -15,19 +24,19 @@ var wrong = 0;
 var count1 = 0;
 var count2 = 0
 var timeRemaining = 20;
+var answers = ["3", "1", "4", "4", "4", "1", "4", "2", "2", "3"];
 var answered = [];
 
 // Creates question and answer text, as well as resets timeRemaining variable
-function createQAndA () {
+function createQAndA() {
     $('#question').html("<h3>" + qAndA[count1][0] + "</h3>");
-    $('#1').text(qAndA[count1][1]);
-    $('#2').text(qAndA[count1][2]);
-    $('#3').text(qAndA[count1][3]);
-    $('#4').text(qAndA[count1][4]);
+    $('#1').html("<p>" + qAndA[count1][1] + "</p>");
+    $('#2').html("<p>" + qAndA[count1][2] + "</p>");
+    $('#3').html("<p>" + qAndA[count1][3] + "</p>");
+    $('#4').html("<p>" + qAndA[count1][4] + "</p>");
     timeRemaining = 20;
     $('#timeCount').text(timeRemaining);
     count1++;
-    console.log(count1);
 };
 createQAndA();
 
@@ -38,24 +47,62 @@ var time = setInterval(countDown, 1000);
 function countDown() {
     timeRemaining--;
     $('#timeCount').text(timeRemaining);
-    if (timeRemaining === 0) {
+    if (timeRemaining <= 0 && count1 === 10) {
+        clearInterval(time);
+        finalScreen();
+    } else if (timeRemaining <= 0) {
         clearInterval(time);
         loadingScreen();
     }
 }
 
 // Stops the timer if an answer is chosen. If answer is chosen and all the questions are done, it sends the game to the final screen. If just the answer is made, sends game to the loading screen. Clears the timer since user has answered (prevents negative number time).
-$('.button').on("click", function() {
-    clearInterval(time)
-    if (count1 === 9) {
+$('.button').on("click", function(event) {
+    console.log(event.currentTarget.id);
+    answered.push(event.currentTarget.id);
+    console.log(answered);
+    clearInterval(time);
+    if (count1 === 10) {
         finalScreen();
+    } else {
+        loadingScreen();
     }
-    loadingScreen();
 })
-
-function loadingScreen () {
-    $('.answers').empty();
-    $('#question').html("<h3>" + "Correctly answered: " + correct + "</br>" + "Incorrectly answered: " + wrong + "</h3>");
+// Screen loaded when user has answered a question or run out of time - only if not all questions have been answered
+function loadingScreen() {
+    $('.answers > div').html("");
+    if (answered[count1 - 1] == answers[count1 - 1]) {
+        $('#question').html("<h3>You answered correctly!!!</h3>");
+        correct++;
+    } else {
+        $('#question').html("<h3>You answered incorrectly</h3>");
+        wrong++;
+    }
     setTimeout(createQAndA, 3000);
     setTimeout(restartTime, 3000);
+}
+
+// Restarts time once loading screen is done.
+function restartTime() {
+    time = setInterval(countDown, 1000);
+}
+
+function finalScreen() {
+    $('.answers > div').html("");
+    if (answered[count1 - 1] === answers[count1 - 1]) {
+        correct++;
+    } else {
+        wrong++;
+    }
+    $('#question').html("<h3>" + "Correctly answered: " + correct + "</br>" + "Incorrectly answered: " + wrong + "</br>" + "Game will restart in ten seconds" + "</h3>");
+    setTimeout(restartGame, 9000);
+    setTimeout(createQAndA, 10000);
+    setTimeout(restartTime, 10000);
+}
+
+function restartGame() {
+    count1 = 0;
+    correct = 0;
+    wrong = 0;
+    answered.length = 0;
 }
